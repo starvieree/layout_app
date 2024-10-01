9d8056e0019e7a0f7d3c8fbdce009df15b4c93e2
starvieree <ainurreza90788@gmail.com>
2024-10-01 11:11:24 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,144 +12,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Template apps',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+          title: const Text('Template apps'),
         ),
-        body: _buildStack(),
-      ),
-    );
-  }
-
-  Widget _buildCard() {
-    return SizedBox(
-      height: 210,
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text(
-                '1625 Main Street',
-                style: TextStyle(fontWeight: FontWeight.w500),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Image(
+                  image: AssetImage('assets/images/pp.jpeg'),
+                ),
               ),
-              subtitle: const Text('My City, CA 99984'),
-              leading: Icon(
-                Icons.restaurant_menu,
-                color: Colors.blue[500],
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova. '
+                  'Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
               ),
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text(
-                '(408) 555-1212',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              leading: Icon(
-                Icons.contact_phone,
-                color: Colors.blue[500],
-              ),
-            ),
-            ListTile(
-              title: const Text('costa@example.com'),
-              leading: Icon(
-                Icons.contact_mail,
-                color: Colors.blue[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildStack() {
-    return Stack(
-      alignment: const Alignment(0.6, 0.6),
-      children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage('assets/images/pp.jpeg'),
-          radius: 100,
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.black45,
-          ),
-          child: const Text(
-            'Mia B',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+              RatingWidget(),
+              DescTextStyle(),
+            ],
           ),
         ),
-      ],
-    );
-  }
-
-  Widget _buildList() {
-    return ListView(
-      children: [
-        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
-        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
-        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
-        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
-        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
-            Icons.theaters),
-        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-        const Divider(),
-        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
-        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
-        _tile('La Ciccia', '291 30th St', Icons.restaurant),
-      ],
-    );
-  }
-
-  ListTile _tile(String title, String subtitle, IconData icon) {
-    return ListTile(
-      title: Text(title,
-          style: const TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 20,
-          )),
-      subtitle: Text(subtitle),
-      leading: Icon(
-        icon,
-        color: Colors.blue[500],
       ),
     );
   }
-
-  Widget buildRow() => const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-        ],
-      );
-
-  Widget buildColumn() => const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-          Expanded(child: Image(image: AssetImage('assets/images/pp.jpeg'))),
-        ],
-      );
-
-  Widget packingWidget() => Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(Icons.star, color: Colors.green[500]),
-          Icon(Icons.star, color: Colors.green[500]),
-          Icon(Icons.star, color: Colors.green[500]),
-          const Icon(Icons.star, color: Colors.black),
-          const Icon(Icons.star, color: Colors.black),
-        ],
-      );
 }
 
 class RatingWidget extends StatelessWidget {
